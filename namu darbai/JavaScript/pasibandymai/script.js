5d9ebc45f4bbab10f2896994a3ee8e9c2ae6cc40
Mantas <mgineika@yahoo.com>
2019-09-18 10:39:41 +0300

    website

--- namu darbai/JavaScript/pasibandymai/script.js
+++
@@ -1,8 +0,0 @@
-var masyvas = ["vienas", "du", "trys", "keturi", "penki"];
-masyvas.push("sesi", "septyni");
-var septyni = masyvas.indexOf("trys");
-
-console.log(masyvas.length);
-
-var a = 2.5974;
-console.log(Math.round(a));
